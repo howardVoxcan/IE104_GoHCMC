f6e2da6d7d3d0c168813fed661f826bf6a6289fe
ThanhTris <thanhtri005@gmail.com>
2025-10-14 16:55:28 +0000

    chore(settings): configure static files for CSS loading

--- GoHCMC/GoHCMC/settings.py
+++ GoHCMC/GoHCMC/settings.py
@@ -44,8 +44,7 @@ INSTALLED_APPS = [
     'location',
     'weather',
     'trip',
-    'favourite',
-    'home'
+    'favourite'
     # 'dialogflow',
 ]
 
@@ -126,6 +125,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field

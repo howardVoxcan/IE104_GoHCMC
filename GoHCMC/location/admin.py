7fbbc4e60194e6a2a2301f349528ae9ade52e9f7
howardVoxcan <vqnhoang97tn@gmail.com>
2025-10-29 14:37:38 +0700

    Models' creation

--- GoHCMC/location/admin.py
+++ GoHCMC/location/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Location, Location_List, Comment
 
 # Register your models here.
+admin.site.register(Location)
+admin.site.register(Location_List)
+admin.site.register(Comment)

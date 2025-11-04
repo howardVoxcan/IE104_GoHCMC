383835fed3dbf252e406a53c06b8a90d9d049372
Thanh Tris <thanhtri005@gmail.com>
2025-11-04 02:57:53 +0000

    feat(location): add locations page and navigation links

--- GoHCMC/location/urls.py
+++ GoHCMC/location/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.homepage, name='homepage'),
+    path('locations/', views.locations, name='locations'),
+    path('locations/<int:id>/', views.location_detail, name='location_detail'),
 ]
--- GoHCMC/location/views.py
+++ GoHCMC/location/views.py
@@ -5,3 +5,9 @@ from django.contrib.auth.decorators import login_required
 
 def homepage(request):
     return render(request, "page/home/index.html")
+def locations(request):
+    # Logic to retrieve and display a list of locations
+    return render(request, "page/locations/index.html")
+def location_detail(request, id):
+    # Logic to retrieve and display details of a specific location by id
+    return render(request, "page/locations/detail.html", {"location_id": id})

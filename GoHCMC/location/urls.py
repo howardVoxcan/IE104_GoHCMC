e0bf51ef1fc21409532175f4e97270c58f3889fc
Thanh Tris <thanhtri005@gmail.com>
2025-11-09 03:02:45 +0000

    feat: add detail view for Location page

--- GoHCMC/location/urls.py
+++ GoHCMC/location/urls.py
@@ -4,6 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.homepage, name='homepage'),
     path('locations', views.locations, name = 'locations'),
-    path('<str:location_code>/',views.display_location, name='display_location'),
+    path('locations/<str:location_code>/',views.location_detail, name='display_location'),
     path('<str:location_code>/submit_comment_ajax/', views.submit_comment_ajax, name='submit_comment_ajax'),
+    # path('locations/<int:id>/', views.location_detail, name='location_detail'),
 ]

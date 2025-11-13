346dda363d7cec9857a60587430bf8cfd62ff1fc
howardVoxcan <vqnhoang97tn@gmail.com>
2025-11-13 13:24:33 +0700

    Favourite page customization and trip page creation

--- GoHCMC/GoHCMC/urls.py
+++ GoHCMC/GoHCMC/urls.py
@@ -26,15 +26,12 @@ urlpatterns = [
     # path('register/', v.register, name = "register"),
     # path('login/', v.custom_login, name='login'),
     path('', include('register.urls')),
-
     path('weather/', include('weather.urls')),
-    path('trip/', include('trip.urls')),
     # path('dialogflow/', include('dialogflow.urls')),
     path('favourite/', include('favourite.urls')),
+    path('trip/', include('trip.urls')),
     path('', include('location.urls')),
-
     path('', include('django.contrib.auth.urls')),
-
 ]
 
 if settings.DEBUG:

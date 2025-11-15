0df3f298ba398666a3725217f5315b95190ba4d4
howardVoxcan <vqnhoang97tn@gmail.com>
2025-11-15 13:28:42 +0700

    Prepare for render deploy: runtime, render.yaml, requirements

--- GoHCMC/settings.py
+++ GoHCMC/settings.py
@@ -38,7 +38,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'register.apps.RegisterConfig',
-    'widget_tweaks',
     'location',
     'weather',
     'trip',
